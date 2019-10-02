269c254c263f0da74713b58a1ef756d010b959e4
leekheang <ly.kheang.jr@gmail.com>
2019-10-02 10:19:30 +0700

    is_chain_valid

--- blockchain.py
+++ blockchain.py
@@ -10,6 +10,7 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1 , previous_hash = '0')
+        
     def create_block(self, proof , previous_hash):
         block = { 'index' : len(self.chain) + 1,
                   'timestamp' : str(datetime.datetime.now()),
@@ -18,6 +19,7 @@ class Blockchain:
         }
         self.chain.append(block)
         return block
+
     def get_previous_block(self):
         return self.chain = [-1]
 
@@ -25,7 +27,7 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation  = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode().hexdigest())
+            hash_operation  = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation [:4] == '0000':
                 check_proof = True
             else: 
@@ -36,3 +38,19 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys= true).encode()
         return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain): 
+        previous_block= chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain['block_index']
+            if block = chain['previous_block'] != self.hash(previous_block):
+                return False
+            previous_proof  = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
